850e30993417f593ea3e1181d69c33565f0913c6
NafisaHassan114 <nafisanafta469@gmail.com>
2024-03-11 17:14:22 +0300

    this is js old and even numbers

---
+++ main.js
@@ -0,0 +1,13 @@
+//code display even numbers
+let even = 2
+while(even <=20){
+
+        console.log(even);
+        even+=2; 
+}
+// code display old numbers
+let old = 1
+while(old <=19){
+        console.log(old);
+        old+=2; 
+}
